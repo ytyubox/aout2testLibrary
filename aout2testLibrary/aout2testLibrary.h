ba44f94bb89a2f4e7e20f1ab7f69b5dd51fa546d
Tsungyu Yu <ytyubox@gmail.com>
2020-12-27 21:22:30 +0800

    Initial Commit

---
+++ aout2testLibrary/aout2testLibrary.h
@@ -0,0 +1,21 @@
+//
+/* 
+ *		Created by 游諭 in 2020/12/27
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 11.1
+ */
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for aout2testLibrary.
+FOUNDATION_EXPORT double aout2testLibraryVersionNumber;
+
+//! Project version string for aout2testLibrary.
+FOUNDATION_EXPORT const unsigned char aout2testLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aout2testLibrary/PublicHeader.h>
+
+
---
+++ aout2testLibraryTests/aout2testLibraryTests.swift
@@ -0,0 +1,36 @@
+//
+/* 
+ *		Created by 游諭 in 2020/12/27
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 11.1
+ */
+
+
+import XCTest
+@testable import aout2testLibrary
+
+class aout2testLibraryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
